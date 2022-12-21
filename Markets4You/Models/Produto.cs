54e9fc6c7232a80cfa73686f7ff368c800782316
JoaoNovais181 <73496007+JoaoNovais181@users.noreply.github.com>
2022-12-21 10:16:00 +0000

    oi

---
+++ Markets4You/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Markets4You.Models
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+
+        public string NomeProduto { get; set;}
+
+        public string Categoria { get; set;}
+
+        public string Descricao { get; set;}
+
+        public int IDEmpresa { get; set;}
+
+        public Produto()
+        {
+
+        }
+    }
+}
